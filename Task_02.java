5a370cc249c000b1978e32d5d138f35186772277
Lupin <skorpion_94@list.ru>
2023-03-04 16:45:35 +0100

    Проверка на полиндром

---
+++ Task_02.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/*
+ * Напишите метод, который принимает на вход строку (String) 
+ * и определяет является ли строка палиндромом (возвращает boolean значение).
+ * 
+ */
+
+public class Task_02 {
+   public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Write string: ");
+        String str = scr.nextLine();
+        
+        System.out.println(isPolindrom(str));
+        
+        scr.close();
+   }
+   private static boolean isPolindrom(String str){
+        for (int i = 0; i < str.length() - i; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+   } 
+}
