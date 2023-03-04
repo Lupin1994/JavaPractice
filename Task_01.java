6bb349f756727cde75d0d4abf2f673d281a69458
Lupin <skorpion_94@list.ru>
2023-03-04 16:22:57 +0100

    Метод выводящий строку определенной длинны

---
+++ Task_01.java
@@ -0,0 +1,23 @@
+/*
+ *  Дано четное число N (>0) и символы c1 и c2.
+    Написать метод, который вернет строку длины N, 
+    которая состоит из чередующихся символов c1 и c2, начиная с c1.
+ * 
+ */
+public class Task_01 {
+    public static void main(String[] args) {
+        char c1 = 'x';
+        char c2 = 'a';
+        int num = 10;
+        System.out.println(createAlternationString(c1, c2, num));
+    }
+
+    private static String createAlternationString (char c1,char c2,int num) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            if(i%2 == 0) result.append(c1);
+            else result.append(c2);
+        }  
+        return result.toString(); 
+    }
+}
